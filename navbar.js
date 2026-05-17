d6a600c52b1f9745282d840168859616e05f8f1a
˜Luis Lima <luislimasdev@gmail.com>
2026-05-17 07:35:17 -0300

    deixar o menu estático nas paginas por meio de JS

---
+++ navbar.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHTML = `
+    <nav class="navbar">
+        <div class="logo-wrapper">
+            <div class="logo-text">LUÍS LIMA
+                <span>LAB ONLINE</span>
+            </div>
+            <div class ="divider"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">Solicitar Design</a></li>
+            <li><a href="#">Acompanhar Pedido</a></li>
+            <li><a href="contatosuporte.html">Contato & Suporte</a></li>
+            <li><a href="#">Sobre nós</a></li>
+        </ul>
+    </nav>
+    `;
+
+    // Injeta a nav bar no elemento que tiver a classe 'meu-menu''
+document.querySelector(".meu-menu").innerHTML = navbarHTML
+});
